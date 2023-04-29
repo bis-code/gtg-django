981283e23272454b5c7629fa021c110dc3ebbed8
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-04-29 15:25:42 +0200

    setup project

---
+++ gtgsite/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+# from models import Coffe, Drink, Capacity
+from .models import Book
+admin.site.register(Book)
+# admin.site.register(Coffe)
+# admin.site.register(Drink)
+# admin.site.register(Capacity)
+# Register your models here.
